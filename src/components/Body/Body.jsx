c73098f62edca0f3e75834a11ffc89504de1ceb7
Safoun <safoun.official@gmail.com>
2023-04-04 11:51:50 +0600

    added a line chart

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import DashBoard from '../dashBoard/DashBoard';
 
 const Body = () => {
     return (
         <div className='root'>
             <div className='display-1 text-center'>Body</div>
+            <DashBoard></DashBoard>
         </div>
     );
 };
