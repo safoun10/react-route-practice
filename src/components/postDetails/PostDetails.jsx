5dd619fbc0fe28c487583889b57a2ce790a3fc2b
Safoun <safoun.official@gmail.com>
2023-04-05 22:54:11 +0600

    navigation functionality added with go back portion

--- src/components/postDetails/PostDetails.jsx
+++ src/components/postDetails/PostDetails.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
     const postDetails = useLoaderData();
     const {body , id} = postDetails;
+
+    const navigate = useNavigate();
+    const goBackHandler = () => {
+        navigate(-1);
+    }
     return (
-        <div className='mx-5 my-5'>
+        <div className='mx-auto pt-5 mt-5 w-50'>
             <dir>Post ID : {id}</dir>
             <div>{body}</div>
+            <button onClick={goBackHandler} className='btn btn-primary w-100 mt-3'>go back</button>
         </div>
     );
 };
